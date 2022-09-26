b5c6a25308a5a6921132c4ae86edaf128cad36fe
Mick Semb Wever <mck@apache.org>
2022-09-27 01:03:32 +0200

    Harden JMX by resolving beanshooter issues

--- src/java/org/apache/cassandra/config/CassandraRelevantProperties.java
+++ src/java/org/apache/cassandra/config/CassandraRelevantProperties.java
@@ -99,6 +99,7 @@ public enum CassandraRelevantProperties
     /** Cassandra jmx remote and local port */
     CASSANDRA_JMX_REMOTE_PORT("cassandra.jmx.remote.port"),
     CASSANDRA_JMX_LOCAL_PORT("cassandra.jmx.local.port"),
+    CASSANDRA_JMX_SECURITY_PROFILE("cassandra.jmx.security.profile", CassandraJmxSecurityProfile.getDefault().toString()),
 
     /** This property  indicates whether SSL is enabled for monitoring remotely. Default is set to false. */
     COM_SUN_MANAGEMENT_JMXREMOTE_SSL ("com.sun.management.jmxremote.ssl"),
